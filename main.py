f79fdb4053af602d126b3cf6cf117889e73c1e46
marie ;) <72861353+igmarie@users.noreply.github.com>
2021-09-21 11:57:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-M
+def Airportlist():
+    import csv
+    dataline = ' '
+    f = open('Airports.txt','r')
+    for row in csv.reader(f):
+        dataline= [row[0],row[1],row[2],row[3]]
+        Airports.append(dataline)
+    f.close()
+    print(Airports)
+
+def Menu()
+    print('Please choose one of the following: ')
+    print('[1] Input airport details')
+    print('[2] Input flight details')
+    print('[3] Input price')
+    print('[4] Calculate profit')
+    print('[Q] To Quit')
+    choice = input()
+    while choice !='Q':
+        if choice == '1':
+            print('Details for airport')
+        elif choice == '2'
+            print('Details for flight')
+        elif choice == '2':
+            print('Price')
+        elif choice == '4':
+            print('Calculating the profit')
+        elif:
+            ('please choose a option')
+            
+    
